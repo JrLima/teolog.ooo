cf4fafe75ed3248cf6ce93a03853fe21946e793c
Emanuel Lima <emanuel.lima@seati.ma.gov.br>
2022-01-31 19:24:53 -0300

    hello world

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,43 +8,43 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Como jogar" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the WORDLE in 6 tries. After each guess, the color of the tiles
-        will change to show how close your guess was to the word.
+        Descubra a palavra certa em 6 tentativas. Depois de cada tentativa, as
+  cores mostram o quão perto você está da solução.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
-        <Cell value="A" />
+        <Cell value="T" status="correct" />
+        <Cell value="U" />
         <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="M" />
+        <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
-        <Cell value="O" />
-        <Cell value="T" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        A letra T faz parte da palavra e está na posição correta.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="V" />
+        <Cell value="I" />
+        <Cell value="O" status="present" />
+        <Cell value="L" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell value="U" status="absent" />
-        <Cell value="E" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        A letra O faz parte da palavra mas em outra posição.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="P" />
+        <Cell value="U" />
+        <Cell value="L" />
+        <Cell value="G" status="absent" />
+        <Cell value="A" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        A letra G não faz parte da palavra.
       </p>
     </BaseModal>
   )
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,19 +1,19 @@
-export const WORDLE_TITLE = 'Not Wordle'
+export const WORDLE_TITLE = 'Teolog.ooo'
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const ABOUT_GAME_MESSAGE = 'About this game'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['Vitória com sabor de mel!', 'Vai haver festa no Céu!', 'Shey! 🔥', 'Pedrada no Inferno!', 'Êh, marreta santa!']
+export const GAME_COPIED_MESSAGE = 'Copiado para área de transferência'
+export const ABOUT_GAME_MESSAGE = 'Sobre este jogo'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Letras insuficientes'
+export const WORD_NOT_FOUND_MESSAGE = 'Palavra não encontrada'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `A palavra era ${solution}`
 export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+export const DELETE_TEXT = 'Apagar'
+export const STATISTICS_TITLE = 'Estatísticas'
+export const GUESS_DISTRIBUTION_TEXT = 'Distribuição de tentativas'
+export const NEW_WORD_TEXT = 'Nova palavra em'
+export const SHARE_TEXT = 'Compartilhar'
+export const TOTAL_TRIES_TEXT = 'jogos'
+export const SUCCESS_RATE_TEXT = 'de vitórias'
+export const CURRENT_STREAK_TEXT = 'vitórias consecutivas'
+export const BEST_STREAK_TEXT = 'melhor sequência'
